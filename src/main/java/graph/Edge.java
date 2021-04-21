e36e2e47e1cdfb0fe5300eafa32cac7764186d35
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-21 11:07:36 +0200

    update: .gitignore, Edge, Vertex

--- src/main/java/graph/Edge.java
+++ src/main/java/graph/Edge.java
@@ -1,11 +1,35 @@
 package graph;
 
+import org.chocosolver.solver.variables.IntVar;
+
 public class Edge {
     private Vertex v1;
     private Vertex v2;
+    private IntVar modelId;
+    private String id;
 
-    public Edge (Vertex v1, Vertex v2){
+    public Edge (String id, Vertex v1, Vertex v2) {
         this.v1 = v1;
         this.v2 = v2;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public IntVar getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(IntVar modelId) {
+        this.modelId = modelId;
+    }
+
+    public Vertex getV1() {
+        return v1;
+    }
+
+    public Vertex getV2() {
+        return v2;
     }
 }
--- src/main/java/graph/Vertex.java
+++ src/main/java/graph/Vertex.java
@@ -1,9 +1,25 @@
 package graph;
 
-public class Vertex {
-    private int id;
+import org.chocosolver.solver.variables.IntVar;
 
-    public Vertex(int id) {
+public class Vertex {
+
+    private String id;
+    private IntVar varId;
+
+    public Vertex(String id) {
         this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public IntVar getVarId() {
+        return varId;
+    }
+
+    public void setVarId(IntVar varId) {
+        this.varId = varId;
     }
 }
