18836d0357adc2d2e8e6920f2f577cc7f9411820
maxiior <59049370+maxiior@users.noreply.github.com>
2021-04-21 14:56:10 +0200

    checking if solution ex

--- src/main/java/labeler/LabelingApp.java
+++ src/main/java/labeler/LabelingApp.java
@@ -14,22 +14,26 @@ public class LabelingApp {
         int n = 3;
         String[] E = {"v1", "v2", "v2", "v3", "v3", "v4"};
 
-        for(int i=0; i<=n; i++)
+        if(n+1 >= E.length/2 +1)
+        {
+            for(int i=0; i<=n; i++)
             graph.addVertex(new Vertex("v"+String.valueOf(i+1)));
-        for(int i = 0, j=0; i<n; i++, j+=2)
-            graph.addEdge(new Edge("e" + String.valueOf(i+1), graph.getVertex(E[j]), graph.getVertex(E[j+1])));
+            for(int i = 0, j=0; i<n; i++, j+=2)
+                graph.addEdge(new Edge("e" + String.valueOf(i+1), graph.getVertex(E[j]), graph.getVertex(E[j+1])));
 
-        SkolemGracefulLabeler labeler = new SkolemGracefulLabeler();
-        Solver solver = labeler.labelGraph(graph);
+            SkolemGracefulLabeler labeler = new SkolemGracefulLabeler();
+            Solver solver = labeler.labelGraph(graph);
 
-        if(solver.solve()){
-            System.out.println("Solution: ");
-            ArrayList<Vertex> vertices = graph.getVertices();
-            ArrayList<Edge> edges = graph.getEdges();
-            for (Vertex vertex: vertices)
-                System.out.printf("Vertex %s\n",vertex.getVarId());
-            for (Edge edge: edges)
-                System.out.printf("Edge %s\n",edge.getVarId());
+            if(solver.solve()){
+                System.out.println("Solution: ");
+                ArrayList<Vertex> vertices = graph.getVertices();
+                ArrayList<Edge> edges = graph.getEdges();
+                for (Vertex vertex: vertices)
+                    System.out.printf("Vertex %s\n",vertex.getVarId());
+                for (Edge edge: edges)
+                    System.out.printf("Edge %s\n",edge.getVarId());
+            }
+            else System.out.println("No solution");
         }
         else System.out.println("No solution");
     }
